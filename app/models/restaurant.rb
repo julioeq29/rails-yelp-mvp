cc8c772bfe18d357f0e79cf1870585a5231d6bb0
Julio Quintana <julioeq29@gmail.com>
2019-02-20 12:31:51 +0000

    validations working fine

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,8 @@
 class Restaurant < ApplicationRecord
+  #if we delete this restaurant, this will delete the associated reviews
+  has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true
+  validates :phone_number, presence: true
   validates :address, presence: true
   validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
