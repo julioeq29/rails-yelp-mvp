175d859c4dd1d0599da259407b3d6f6e34871df7
Julio Quintana <julioeq29@gmail.com>
2019-02-20 14:33:42 +0000

    created routes & nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   # A visitor can add a new review to a restaurant
+  resources :restaurants, only: [ :index, :new, :show, :create ] do
+    # No need for :new, as i dont need a new page to create a review
+    resources :reviews, only: [ :create ]
+  end
+
 end
