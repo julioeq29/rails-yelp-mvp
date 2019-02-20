7d14d28af95d1b67c98002d74017e7a1e3fbabb4
Julio Quintana <julioeq29@gmail.com>
2019-02-20 12:47:34 +0000

    added seeds w 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurants_attributes = [
+  {
+    name: 'Something & Something',
+    address: '84 Finsbury Park',
+    category: 'belgian',
+    phone_number: '07 08 36 52 21'
+  },
+  {
+    name: 'Bufi',
+    address: '70 Hackney Road',
+    category: 'italian',
+    phone_number: '08 35 21 98 31'
+  },
+  {
+    name: 'Beijing Dumpling',
+    address: '123 Leicester Square',
+    category: 'chinese',
+    phone_number: '03 78 21 09 43'
+  },
+  {
+    name: 'Oh la la!',
+    address: '9 Soho',
+    category: 'french',
+    phone_number: '07 03 43 43 89'
+  },
+  {
+    name: 'Maki Maki',
+    address: '45 English Road',
+    category: 'japanse',
+    phone_number: '07 90 34 75 12'
+  }
+]
